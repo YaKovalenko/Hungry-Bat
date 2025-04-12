c61fabed83f3d5162caaa6a3f400419f3e2074bd
yakovalenko <yanakovalenko.space@gmail.com>
2025-04-12 11:59:22 +0300

    Add gameplay view

---
+++ Assets/Scripts/UI/Views/GameplayView.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Views
+{
+    public class GameplayView : BaseView
+    {
+        [SerializeField] 
+        private Button _closeButton;
+        
+        [SerializeField]
+        private TextMeshProUGUI _scoreText;
+        
+        [SerializeField]
+        private TextMeshProUGUI _timeText;
+
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(OnCloseButtonClicked);
+        }
+
+        private void OnDestroy()
+        {
+            _closeButton.onClick.RemoveListener(OnCloseButtonClicked);
+        }
+
+        private void OnCloseButtonClicked()
+        {
+            
+        }
+    }
+}
